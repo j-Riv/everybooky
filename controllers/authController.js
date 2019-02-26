ca26b23dc6a219c037b686b5ac117e4ea75a2c89
j-Riv <jose.alfredo.rivera@gmail.com>
2019-02-25 20:40:41 -0800

    Minor Updates

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,18 @@
-var exports = module.exports = {}
-
-exports.signup = function(req, res) {
-    res.render('signup');
-}
-
-exports.signin = function(req, res) {
-    res.render('signin');
-}
-
-exports.dashboard = function(req, res) {
-    res.render('dashboard', {
-        id: req.user.id
-    });
-}
-
-exports.logout = function(req, res) {
-    req.session.destroy(function(err) {
-        res.redirect('/');
-    });
+module.exports = {
+    signup: function(req, res) {
+        res.render('signup');
+    },
+    signin: function(req, res) {
+        res.render('signin');
+    },
+    dashboard: function(req, res) {
+        res.render('dashboard', {
+            id: req.user.id
+        });
+    },
+    logout: function(req, res) {
+        req.session.destroy(function(err) {
+            res.redirect('/');
+        });
+    }
 }
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const passport = require('passport');
 const session = require('express-session');
 const env = require('dotenv').load();
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars');
 
-const models = require("./models");
+const models = require('./models');
 
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // For Passport
 // session secret
@@ -28,12 +30,19 @@ app.use(passport.session());
 
 // Handlebars
 app.engine(
-    "handlebars",
+    'handlebars',
     exphbs({
-        defaultLayout: "main"
+        defaultLayout: 'main'
     })
 );
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
+
+// place this middleware before any other route definitions
+// makes io available as req.io in all request handlers
+app.use(function(req, res, next) {
+    req.io = io;
+    next();
+});
 
 // Routes
 const authRoute = require('./routes/auth.js')(app, passport);
@@ -47,15 +56,15 @@ const syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === 'test') {
     syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
 models.sequelize.sync(syncOptions).then(function() {
-    app.listen(PORT, function() {
+    http.listen(PORT, function() {
         console.log(
-            "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+            '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
             PORT,
             PORT
         );
