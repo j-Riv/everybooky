81b0cad6865c1e65fc5ba45761de9038ed0547a7
TKMCode <tyler.code.maples@gmail.com>
2019-02-27 10:45:07 -0800

    update

--- models/author.js
+++ models/author.js
@@ -1,4 +1,4 @@
-module.exports = function(sequelize, DataTypes) {
+    module.exports = function(sequelize, DataTypes) {
     var Author = sequelize.define("Author", {
         username: {
             type: DataTypes.STRING(20),
